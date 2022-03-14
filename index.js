5ca56d8a9cf1daea70eccc9232073ec46a5ce4ca
Lesley Cheung <lesley.yc@gmail.com>
2022-03-14 13:41:28 +0800

    done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-! function(e, t) { "object" == typeof exports && "object" == typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define([], t) : "object" == typeof exports ? exports.define_me = t() : e.define_me = t() }(this, (function() { return (() => { "use strict"; var e = { d: (t, n) => { for (var o in n) e.o(n, o) && !e.o(t, o) && Object.defineProperty(t, o, { enumerable: !0, get: n[o] }) }, o: (e, t) => Object.prototype.hasOwnProperty.call(e, t), r: e => { "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) } },
+! function(e, t) { "object" == typeof exports && "object" == typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define([], t) : "object" == typeof exports ? exports.define_me = t() : e.define_me = t() }(this, (function() {
+    return (() => {
+        "use strict";
+        var e = { d: (t, n) => { for (var o in n) e.o(n, o) && !e.o(t, o) && Object.defineProperty(t, o, { enumerable: !0, get: n[o] }) }, o: (e, t) => Object.prototype.hasOwnProperty.call(e, t), r: e => { "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) } },
             t = {};
-        e.r(t), e.d(t, { containsThis: () => m, dashDDMMYYYY: () => u, dashYYYYMMDD: () => p, git: () => o, hhmm: () => r, hhmmss: () => d, hyphenDDMMYYYY: () => s, hyphenYYYYMMDD: () => a, knexDate: () => c, matchWords: () => y, node_modules: () => n, stringDate: () => f, yyyymmdd: () => i }); var n = /^(?:.*[\\\/])?node_modules(?:[\\\/].*)?$/,
+        e.r(t), e.d(t, { containsThis: () => m, dashDDMMYYYY: () => u, dashYYYYMMDD: () => p, git: () => o, hhmm: () => r, hhmmss: () => d, hyphenDDMMYYYY: () => s, hyphenYYYYMMDD: () => a, knexDate: () => c, matchWords: () => y, node_modules: () => n, stringDate: () => f, yyyymmdd: () => i });
+        var n = /^(?:.*[\\\/])?node_modules(?:[\\\/].*)?$/,
             o = /^(?:.*[\\\/])?.git(?:[\\\/].*)?$/,
             r = /^(2[0-3]|[01][0-9]):?([0-5][0-9])$/,
             d = /^(2[0-3]|[01][0-9]):?([0-5][0-9]):?([0-5][0-9])$/,
@@ -12,9 +16,17 @@
             f = /(\w{2,}) (\d{1,}),*\s*\d*/g,
             c = /(.{1,})T(.{1,})Z/g;
 
-        function m(e, t) { return function(e) { var t = new RegExp(/^[A-Z]/),
+        function m(e, t) {
+            return function(e) {
+                var t = new RegExp(/^[A-Z]/),
                     n = new RegExp(/^[a-z]/),
                     o = t.test(e),
-                    r = n.test(e); return o && r }(e) && (e = function(e) { return e.charAt(0).toUpperCase() + e.slice(1) }(e)), new RegExp(t).test(e) }
+                    r = n.test(e);
+                return o && r
+            }(e) && (e = function(e) { return e.charAt(0).toUpperCase() + e.slice(1) }(e)), new RegExp(t).test(e)
+        }
 
-        function y(e, t) { for (var n = /[(){[*+?.\\^$|]/g, o = 0; o < t.length; o++) t[o] = t[o].replace(n, "\\$&"); var r = new RegExp("\\b(?:" + t.join("|") + ")\\b", "gi"); return e.match(r) || [] } return t })() }));
+        function y(e, t) { for (var n = /[(){[*+?.\\^$|]/g, o = 0; o < t.length; o++) t[o] = t[o].replace(n, "\\$&"); var r = new RegExp("\\b(?:" + t.join("|") + ")\\b", "gi"); return e.match(r) || [] }
+        return t
+    })()
+}));
